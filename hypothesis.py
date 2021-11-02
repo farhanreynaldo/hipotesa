6343688302d0ecdc8d56d550fce17dfab6e4b760
Farhan Reynaldo <farhanreynaldo@gmail.com>
2021-11-02 14:18:05 +0700

    Add specifier data transformation

--- hypothesis.py
+++ hypothesis.py
@@ -1,14 +1,16 @@
 class Hypothesis:
-    def __init__(self, data, generator, test_statistic, iters=1000):
+    def __init__(self, data, specifier, generator, test_statistic, iters=1000):
         self.data = data
+        self.specifier = specifier
         self.generator = generator
         self.test_statistic = test_statistic
         self.iters = iters
         self.is_simulated = False
 
     def simulate(self):
+        self._data = self.specifier.transform(self.data)
         self.test_stats = [
-            self.test_statistic(self.generator(self.data))
+            self.test_statistic(self.generator(self._data))
             for _ in range(self.iters)
         ]
         self.is_simulated = True
@@ -18,5 +20,5 @@ class Hypothesis:
     def PValue(self):
         if not self.is_simulated:
             raise Exception("Simulation not performed")
-        self.actual = self.test_statistic(self.data)
+        self.actual = self.test_statistic(self._data)
         return sum(self.test_stats >= self.actual) / len(self.test_stats)
