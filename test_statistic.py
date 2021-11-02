81a667216bf8cea8fd0a0db1821a235eef6aab29
Farhan Reynaldo <farhanreynaldo@gmail.com>
2021-11-02 14:17:45 +0700

    Create DiffProps for test statistics

--- test_statistic.py
+++ test_statistic.py
@@ -4,10 +4,22 @@ class TestStatistic:
 
 
 class DiffMeans(TestStatistic):
-    def __init__(self):
+    def __init__(self, direction=None):
         super().__init__()
+        self.direction = direction
 
     def __call__(self, data):
         group1, group2 = data
-        test_stat = abs(group1.mean() - group2.mean())
+        if self.direction is None or self.direction == "both":
+            test_stat = abs(group1.mean() - group2.mean())
+        elif self.direction == "right":
+            test_stat = group1.mean() - group2.mean()
+        elif self.direction == "left":
+            test_stat = group2.mean() - group1.mean()
+        else:
+            raise ValueError("direction must be either right or left")
         return test_stat
+
+
+class DiffProps(DiffMeans):
+    pass
